ad3ea52afc885dfce9c054fec2e0bca578df2162
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-09-26 16:33:06 +0200

    fix: add missing imports

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -641,7 +641,6 @@ _import_structure = {
     ],
     "models.paligemma": ["PaliGemmaConfig"],
     "models.colpali": ["ColPaliConfig"],
-    "models.colpali": ["ColPaliConfig"],
     "models.patchtsmixer": ["PatchTSMixerConfig"],
     "models.patchtst": ["PatchTSTConfig"],
     "models.pegasus": [
@@ -6545,7 +6544,6 @@ if TYPE_CHECKING:
         )
         from .models.colpali import (
             ColPaliModel,
-            ColPaliPreTrainedModel,
             ColPaliProcessor,
         )
         from .models.conditional_detr import (
